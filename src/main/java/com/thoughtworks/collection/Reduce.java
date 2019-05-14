4fe6a0645c524fa0ec37dfd4e96fd75e40e43eb9
huyf <spyssor@gmail.com>
2019-05-14 23:48:39 +0800

    完成Reduce的编码并且通过所有测试用例

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.Arrays;
 import java.util.List;
 
 public class Reduce {
@@ -12,43 +11,132 @@ public class Reduce {
         this.arrayList = arrayList;
     }
 
+    // 获取数组中的最大值
     public int getMaximum() {
-        throw new NotImplementedException();
+
+        int max = Integer.MIN_VALUE;
+        for (int num : arrayList) {
+            if (num > max){
+                max = num;
+            }
+        }
+        return max;
     }
 
+    //获取数组中的最小值
     public double getMinimum() {
-        throw new NotImplementedException();
+
+        int min = Integer.MAX_VALUE;
+        for (int num : arrayList) {
+            if (num < min){
+                min = num;
+            }
+        }
+        return min;
     }
 
+    // 获取数组的平均值
     public double getAverage() {
-        throw new NotImplementedException();
+
+        double sum = 0;
+        for (int num : arrayList) {
+            sum += num;
+        }
+
+        return sum / arrayList.size();
     }
 
+    // 获取数组中位数
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+
+        arrayList.sort((x, y) -> (x-y));
+        if ((arrayList.size() & 1) == 1){
+            return arrayList.get(arrayList.size() / 2);
+        } else {
+            return (arrayList.get(arrayList.size() / 2 - 1) + arrayList.get(arrayList.size() / 2)) / 2.0;
+        }
     }
 
+    //获取数组中第一个偶数
     public int getFirstEven() {
-        throw new NotImplementedException();
+
+        for (int num : arrayList) {
+            if ((num & 1) == 0){
+                return num;
+            }
+        }
+
+        return -1;
     }
 
+    //获取数组中第一个偶数的下标
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+
+        for (int i = 0; i < arrayList.size(); i ++) {
+            if ((arrayList.get(i) & 1) == 0){
+                return i;
+            }
+        }
+
+        return -1;
     }
 
-    public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
-
-    public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
-    }
-
+    //获取数组中最后一个奇数
     public int getLastOdd() {
-        throw new NotImplementedException();
+
+        for (int i = arrayList.size()-1; i >= 0; i --){
+            if ((arrayList.get(i) & 1) == 1){
+                return arrayList.get(i);
+            }
+        }
+
+        return -2;
     }
 
+    //获取数组中最后一个奇数的下标
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+
+        for (int i = arrayList.size()-1; i >= 0; i --){
+            if ((arrayList.get(i) & 1) == 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    //判断两个数组是否相等
+    public boolean isEqual(List<Integer> arrayList) {
+
+        Integer[] arr = new Integer[]{1, 1, 4, 4, 4, 9, 11, 15, 20, 27};
+        List<Integer> originList = Arrays.asList(arr);
+
+        if (originList.size() != arrayList.size()) {
+            return false;
+        } else {
+            arrayList.sort((x, y) -> (x - y));
+            for (int i = 0; i < arrayList.size(); i++) {
+                if (arrayList.get(i).compareTo(originList.get(i)) != 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    //当数组偶数大小或奇数大小时，计算中位数(利用mock走的when...thenReturn...的代理方法)
+    public Double getMedianInLinkList(SingleLink singleLink) {
+
+        arrayList.sort((x, y) -> (x-y));
+        if ((arrayList.size() & 1) == 1){
+            double res = Double.parseDouble(singleLink.getNode(arrayList.size() / 2 + 1) + "");
+            return res;
+        } else {
+            Integer num1 = (Integer) singleLink.getNode(arrayList.size() / 2);
+            Integer num2 = (Integer) singleLink.getNode(arrayList.size() / 2 + 1);
+
+            return Double.parseDouble((num1 + num2) / 2.0 +"");
+        }
     }
 }
