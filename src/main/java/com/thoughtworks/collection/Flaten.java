1b033d54ec0a0f620a5ac0ec8edf57683b296621
huyf <spyssor@gmail.com>
2019-05-14 18:30:25 +0800

    完成Flaten的编码并且通过所有测试用例

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Flaten {
@@ -11,11 +12,35 @@ public class Flaten {
         this.array = array;
     }
 
+    //把二维数组变成一维数组
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i ++){
+            for (int j = 0; j < array[i].length; j ++){
+                res.add(array[i][j]);
+            }
+        }
+
+        return res;
     }
 
+    // 把二维数组变成一维数组,消除重复,按照第一次出现的顺序排列最后的输出结果
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+
+        List<Integer> res = new ArrayList<>();
+        List<Integer> record = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i ++){
+            for (int j = 0; j < array[i].length; j ++){
+                if (!record.contains(array[i][j])) {
+                    res.add(array[i][j]);
+                    record.add(array[i][j]);
+                }
+            }
+        }
+
+        return res;
     }
 }
