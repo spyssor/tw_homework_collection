9235b83a92d9d3974cddaf54e8b0ec5f186d5364
huyf <spyssor@gmail.com>
2019-05-14 22:13:43 +0800

    完成MyMap的编码并且通过所有测试用例

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,23 +17,65 @@ public class MyMap {
         this.array = array;
     }
 
+    //将集合A中得元素映射成集合B中的元素
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+
+        List<Integer> res = new ArrayList<>();
+        for (Integer e : array) {
+            res.add(e*3);
+        }
+        array = res;
+
+        return array;
     }
 
+    //数字映射为字母
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+
+        List<String> res = new ArrayList<>();
+        for (Integer num : array) {
+            if (num >= 1 && num <= 26) {
+                res.add(letterList.get(num - 1));
+            } else {
+                throw new IllegalArgumentException("array's number is out of boundary");
+            }
+        }
+
+        return res;
     }
 
+    //字母表映射 只针对两位字母的映射
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+
+        List<String> res = new ArrayList<>();
+        for (Integer num : array) {
+            if (num >= 1 && num <= 26){
+                res.add(letterList.get(num - 1));
+            } else if (num > 26 && num <= 26*26+26) {
+                int firstLetterNo = (num-1) / 26;
+                int secondLetterNo = (num-1) % 26;
+                res.add(letterList.get(firstLetterNo - 1) + letterList.get(secondLetterNo));
+            } else {
+                throw new IllegalArgumentException("array's number is out of boundary");
+            }
+        }
+
+        return res;
     }
 
+    //从大到小排序
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+
+        array.sort((x, y) -> (y-x));
+
+        return array;
     }
 
+    //从小到大排序
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+
+        array.sort((x, y) -> (x-y));
+
+        return array;
     }
 }
